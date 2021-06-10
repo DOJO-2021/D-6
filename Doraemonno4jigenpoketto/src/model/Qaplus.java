76cea83ac6dab12c943d3aa8394eb0e5ffdf1b24
dojo2021-k-moishita <morisita@seplus2016.onmicrosoft.com>
2021-06-10 18:04:09 +0900

    6月10日森下

--- Doraemonno4jigenpoketto/src/model/Qaplus.java
+++ Doraemonno4jigenpoketto/src/model/Qaplus.java
@@ -1,5 +1,120 @@
 package model;
+import java.io.Serializable;
+import java.util.Date;
+//質問と回答1行分のデータを持つ
 
-public class Qaplus {
+public class Qaplus implements Serializable {
+  private int question_id;  //質問ID
+  private Date date;        //質問した日時
+  private String answerer;    //回答者
+  private String course; //単元項目ID
+  private String unit;
+  private String category_item;
+  private String textbook;
+  private String question; //質問内容
+  private String answer;   //回答内容
+  private int pageview;   //閲覧数
+  private String registrant;//登録者
+
+  //引数のないQaのコンストラクタ
+  public Qaplus() {
+	  super();
+	  this.question_id=0;
+	  this.date=null;
+	  this.answerer="";
+	  this.course="";
+	  this.unit="";
+	  this.textbook="";
+	  this.category_item="";
+	  this.question="";
+	  this.answer="";
+	  this.pageview=0;
+	  this.registrant="";
+  }
+  //引数のあるQaのコンストラクタ
+  public Qaplus(int question_id,Date date,String answerer,String course, String unit, String category_item, String textbook, String question,String answer,int pageview,String registrant) {
+	  super();
+	  this.question_id=question_id;
+	  this.date=date;
+	  this.answerer=answerer;
+	  this.category_item=category_item;
+	  this.course=course;
+	  this.unit=unit;
+	  this.textbook=textbook;
+	  this.question=question;
+	  this.answer=answer;
+	  this.pageview=pageview;
+	  this.registrant=registrant;
+  }
+
+  //getterとsetterの設定
+public int getQuestion_id() {
+	return question_id;
+}
+public void setQuestion_id(int question_id) {
+	this.question_id = question_id;
+}
+public Date getDate() {
+	return date;
+}
+public void setDate(Date date) {
+	this.date = date;
+}
+public String getAnswerer() {
+	return answerer;
+}
+public void setAnswerer(String answerer) {
+	this.answerer = answerer;
+}
+public String getCategory_item() {
+	return category_item;
+}
+public void setCategory_id(String category_item) {
+	this.category_item = category_item;
+}
+public String getQuestion() {
+	return question;
+}
+public void setQuestion(String question) {
+	this.question = question;
+}
+public String getAnswer() {
+	return answer;
+}
+public void setAnswer(String answer) {
+	this.answer = answer;
+}
+
+public int getPageview() {
+	return pageview;
+}
+public void setPageview(int pageview) {
+	this.pageview = pageview;
+}
+public String getRegistrant() {
+	return registrant;
+}
+public void setRegistrant(String registrant) {
+	this.registrant = registrant;
+}
+public String getCourse() {
+	return course;
+}
+public void setCourse(String course) {
+	this.course = course;
+}
+public String getUnit() {
+	return unit;
+}
+public void setUnit(String unit) {
+	this.unit = unit;
+}
+public String getTextbook() {
+	return textbook;
+}
+public void setTextbook(String textbook) {
+	this.textbook = textbook;
+}
+
 
 }
