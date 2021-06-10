2b718466d9c211d256054995877b7cde293ac571
向後海 <kougo@seplus2016.onmicrosoft.com>
2021-06-10 13:49:15 +0900

    javabeans

--- Doraemonno4jigenpoketto/src/model/Category.java
+++ Doraemonno4jigenpoketto/src/model/Category.java
@@ -1,5 +1,58 @@
 package model;
+import java.io.Serializable;
 
-public class Category {
+//単元項目1行分のデータを持つ
+public class Category implements Serializable{
+
+	private int course_id;
+	private int unit_id;
+	private int category_id;
+	private String category_item;
+
+//引数のないCategoryのコンストラクタ
+public Category() {
+	super();
+	this.course_id=0;
+	this.unit_id=0;
+	this.category_id=0;
+	this.category_item="";
+}
+
+
+//引数のあるCategoryのコンストラクタ
+public Category(int course_id,int unit_id, int category_id,String category_item) {
+super();
+this.course_id=course_id;
+this.unit_id=unit_id;
+this.category_id=category_id;
+this.category_item=category_item;
+}
+
+//getterとsetterの設定
+	public int getCourse_id() {
+		return course_id;
+	}
+	public void setCourse_id(int course_id) {
+		this.course_id = course_id;
+	}
+	public int getUnit_id() {
+		return unit_id;
+	}
+	public void setUnit_id(int unit_id) {
+		this.unit_id = unit_id;
+	}
+	public int getCategory_id() {
+		return category_id;
+	}
+	public void setCategory_id(int category_id) {
+		this.category_id = category_id;
+	}
+	public String getCategory_item() {
+		return category_item;
+	}
+	public void setCategory_item(String category_item) {
+		this.category_item = category_item;
+	}
+
 
 }
--- Doraemonno4jigenpoketto/src/model/User.java
+++ Doraemonno4jigenpoketto/src/model/User.java
@@ -1,5 +1,26 @@
 package model;
+import java.io.Serializable;
 
-public class User {
+//事務局ログイン1行分のデータを持つ
+public class User implements Serializable {
+		private String id; //ID
+		private String pass; //pw
 
+//引数のないuserのコンストラクタ
+		public User() {
+			 this.id="";
+			 this.pass="";
+	    }
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id=id;
+		}
+		public String getPass() {
+			return pass;
+		}
+		public void setPass(String pass) {
+			this.pass=pass;
+		}
 }
