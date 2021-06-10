77e0ed9cd4d44a36055aabb1166edab639a1191b
dojo2021-k-moishita <morisita@seplus2016.onmicrosoft.com>
2021-06-10 13:58:14 +0900

    Dao試作

--- Doraemonno4jigenpoketto/src/dao/CategoryDao.java
+++ Doraemonno4jigenpoketto/src/dao/CategoryDao.java
@@ -1,5 +1,90 @@
 package dao;
 
-public class CategoryDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-}
+import model.Category;
+
+
+public class CategoryDao {
+	// 引数paramで検索項目を指定し、検索結果のリストを返す
+	public List<Category> select(Category param) {
+		Connection conn = null;
+		List<Category> cardList = new ArrayList<Category>();
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/D-6/Doraemonno4jigenpoketto/database", "sa", "sa");
+
+			// SQL文を準備する
+			String sql = "select COURSE_ID, UNIT_ID, CATEGORY_ID, CATEGORY_ITEM from CATEGORY "
+					+ "where COURSE_ID=? and UNIT_ID=?;";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			/*if (param.getNumber() != 0) {
+				pStmt.setInt(1,  param.getNumber() );
+			}
+			else {
+				pStmt.setString(1, "%");
+			}*/
+			if (param.getCourse_id() !=0) {
+				pStmt.setInt(1,  param.getCourse_id());
+			}
+			else {
+				pStmt.setInt(1, 6);
+			}
+			if (param.getQuestion2() != 0) {
+				pStmt.setInt(2, param.getUnit_id());
+			}
+			else {
+				pStmt.setInt(2, 1);
+			}
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				Qa card = new Category(
+				rs.getInt("COURSE_ID"),
+				rs.getInt("UNIT_ID"),
+				rs.getInt("CATEGORY_ID"),
+				rs.getString("CATEGORY_ITEM")
+				);
+				cardList.add(card);
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			cardList = null;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			cardList = null;
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					cardList = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return cardList;
+	}
+
+
--- Doraemonno4jigenpoketto/src/dao/QaDao.java
+++ Doraemonno4jigenpoketto/src/dao/QaDao.java
@@ -9,11 +9,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.Qa;
+import model.Qas;
 
 
 public class QaDao {
 	// 引数paramで検索項目を指定し、検索結果のリストを返す
-	public List<Qa> select(Qa param) {
+	public List<Qa> select(Qas param) {
 		Connection conn = null;
 		List<Qa> cardList = new ArrayList<Qa>();
 
@@ -22,15 +23,39 @@ public class QaDao {
 			Class.forName("org.h2.Driver");
 
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/D-6/Doraemonno4jigenpoketto/database", "sa", "sa");
 
 			// SQL文を準備する
-			String sql = "select NUMBER, NAME, NAMEFURIGANA, ENGLISHNAME, COMPANY, SECTION, POST, ZIPCODE, ADDRESS, MOBILENUMBER, TELEPHONENUMBER, FAX, MAILADDRESS, REMARKS from BC "
-					+ "WHERE NAME LIKE ? AND  (CASE WHEN NAMEFURIGANA IS NULL THEN '' ELSE NAMEFURIGANA END) LIKE ? AND (CASE WHEN ENGLISHNAME IS NULL THEN '' ELSE ENGLISHNAME END) LIKE ? AND "
-					+ "(CASE WHEN COMPANY IS NULL THEN '' ELSE COMPANY END) LIKE ? AND (CASE WHEN SECTION IS NULL THEN '' ELSE SECTION END) LIKE ? AND (CASE WHEN POST IS NULL THEN '' ELSE POST END) LIKE ? AND "
-					+ "(CASE WHEN ZIPCODE IS NULL THEN '' ELSE ZIPCODE END) LIKE ? AND (CASE WHEN ADDRESS IS NULL THEN '' ELSE ADDRESS END) LIKE ? AND(CASE WHEN MOBILENUMBER IS NULL THEN '' ELSE MOBILENUMBER END) LIKE ? AND (CASE WHEN TELEPHONENUMBER IS NULL THEN '' ELSE TELEPHONENUMBER END) LIKE ? AND"
-					+ "(CASE WHEN FAX IS NULL THEN '' ELSE FAX END) LIKE ? AND (CASE WHEN MAILADDRESS IS NULL THEN '' ELSE MAILADDRESS END) LIKE ? AND (CASE WHEN REMARKS IS NULL THEN '' ELSE REMARKS END) LIKE ? "
-					+ "ORDER BY NUMBER";
+			String sql = "select Q.QUESTION_ID, Q.DATE, Q.ANSWERER,  Q.CATEGORY_ID, CO.COURSE, U.UNIT, CA.CATEGORY_ITEM,"
+					+ " U.TEXTBOOK, Q.QUESTION, Q.ANSWER, Q.PAGEVIEW, Q.REGISTANT from\r\n"
+					+ " ((QA as Q inner join CATEGORY as CA on Q.CATEGORY_ID=CA.CATEGORY_ID) \r\n"
+					+ "inner join UNIT as U on CA.COURSE_ID=U.COURSE_ID and CA.UNIT_ID=U.UNIT_ID) \r\n"
+					+ "inner join COURSE as CO on CA.COURSE_ID=CO.COURSE_ID "
+					+ "WHERE CONCAT((CASE WHEN Q.ANSWERER IS NULL THEN '' ELSE Q.ANSWERER END),  "
+					+ "(CASE WHEN CO.COURSE IS NULL THEN '' ELSE CO.COURSE END), "
+					+ "(CASE WHEN U.UNIT IS NULL THEN '' ELSE U.UNIT END), "
+					+ "(CASE WHEN CA.CATEGORY_ITEM IS NULL THEN '' ELSE CA.CATEGORY_ITEM END), "
+					+ "(CASE WHEN U.TEXTBOOK IS NULL THEN '' ELSE U.TEXTBOOK END), "
+					+ "(CASE WHEN Q.QUESTION IS NULL THEN '' ELSE Q.QUESTION END), "
+					+ "(CASE WHEN Q.ANSWER IS NULL THEN '' ELSE Q.ANSWER END), "
+					+ "(CASE WHEN Q.REGISTANT IS NULL THEN '' ELSE Q.REGISTANT END)) LIKE ?"
+					+ "AND CONCAT((CASE WHEN Q.ANSWERER IS NULL THEN '' ELSE Q.ANSWERER END),  "
+					+ "(CASE WHEN CO.COURSE IS NULL THEN '' ELSE CO.COURSE END), "
+					+ "(CASE WHEN U.UNIT IS NULL THEN '' ELSE U.UNIT END), "
+					+ "(CASE WHEN CA.CATEGORY_ITEM IS NULL THEN '' ELSE CA.CATEGORY_ITEM END), "
+					+ "(CASE WHEN U.TEXTBOOK IS NULL THEN '' ELSE U.TEXTBOOK END), "
+					+ "(CASE WHEN Q.QUESTION IS NULL THEN '' ELSE Q.QUESTION END), "
+					+ "(CASE WHEN Q.ANSWER IS NULL THEN '' ELSE Q.ANSWER END), "
+					+ "(CASE WHEN Q.REGISTANT IS NULL THEN '' ELSE Q.REGISTANT END)) LIKE ?"
+					+ "AND CONCAT((CASE WHEN Q.ANSWERER IS NULL THEN '' ELSE Q.ANSWERER END),  "
+					+ "(CASE WHEN CO.COURSE IS NULL THEN '' ELSE CO.COURSE END), "
+					+ "(CASE WHEN U.UNIT IS NULL THEN '' ELSE U.UNIT END), "
+					+ "(CASE WHEN CA.CATEGORY_ITEM IS NULL THEN '' ELSE CA.CATEGORY_ITEM END), "
+					+ "(CASE WHEN U.TEXTBOOK IS NULL THEN '' ELSE U.TEXTBOOK END), "
+					+ "(CASE WHEN Q.QUESTION IS NULL THEN '' ELSE Q.QUESTION END), "
+					+ "(CASE WHEN Q.ANSWER IS NULL THEN '' ELSE Q.ANSWER END), "
+					+ "(CASE WHEN Q.REGISTANT IS NULL THEN '' ELSE Q.REGISTANT END)) LIKE ?"
+					+ "ORDER BY Q.PAGEVIEW";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
@@ -40,83 +65,23 @@ public class QaDao {
 			else {
 				pStmt.setString(1, "%");
 			}*/
-			if (param.getName() != null && param.getName() != "") {
-				pStmt.setString(1, "%" + param.getName() + "%");
+			if (param.getQuestion1() != null && param.getQuestion1() != "") {
+				pStmt.setString(1, "%" + param.getQuestion1() + "%");
 			}
 			else {
 				pStmt.setString(1, "%");
 			}
-			if (param.getNamefurigana() != null && param.getNamefurigana() != "") {
-				pStmt.setString(2, "%" + param.getNamefurigana() + "%");
+			if (param.getQuestion2() != null && param.getQuestion2() != "") {
+				pStmt.setString(2, "%" + param.getQuestion2() + "%");
 			}
 			else {
 				pStmt.setString(2, "%");
 			}
-			if (param.getEnglishname() != null && param.getEnglishname() != "") {
-				pStmt.setString(3, "%" + param.getEnglishname() + "%");
+			if (param.getQuestion3() != null && param.getQuestion3() != "") {
+				pStmt.setString(3, "%" + param.getQuestion3() + "%");
 			}
 			else {
 				pStmt.setString(3, "%");
-			}
-			if ( param.getCompany() !=null &&param.getCompany() != "") {
-				pStmt.setString(4, "%" + param.getCompany() + "%");
-			}
-			else {
-				pStmt.setString(4,"%");
-			}
-			if (param.getSection() != null && param.getSection() != "") {
-				pStmt.setString(5, "%" + param.getSection() + "%");
-			}
-			else {
-				pStmt.setString(5, "%");
-			}
-			if (param.getPost() != null && param.getPost() != "") {
-				pStmt.setString(6, "%" + param.getPost() + "%");
-			}
-			else {
-				pStmt.setString(6,"%");
-			}
-			if (param.getZipcode() != null && param.getZipcode() != "") {
-				pStmt.setString(7, "%"+param.getZipcode()+"%");
-			}
-			else {
-				pStmt.setString(7,"%");
-			}
-			if (param.getAddress() != null && param.getAddress() != "") {
-				pStmt.setString(8, "%" + param.getAddress() + "%");
-			}
-			else {
-				pStmt.setString(8, "%");
-			}
-			if (param.getMobilenumber() != null && param.getMobilenumber() != "") {
-				pStmt.setString(9, "%"+param.getMobilenumber()+"%");
-			}
-			else {
-				pStmt.setString(9, "%");
-			}
-			if (param.getTelephonenumber() != null && param.getTelephonenumber() != "") {
-				pStmt.setString(10, "%"+param.getTelephonenumber()+"%");
-			}
-			else {
-				pStmt.setString(10,"%");
-			}
-			if (param.getFax() != null && param.getFax() != "") {
-				pStmt.setString(11, "%"+param.getFax()+"%");
-			}
-			else {
-				pStmt.setString(11,"%");
-			}
-			if (param.getMailaddress() != null && param.getMailaddress() != "") {
-				pStmt.setString(12, "%" + param.getMailaddress() + "%");
-			}
-			else {
-				pStmt.setString(12,"%");
-			}
-			if (param.getRemarks() != null && param.getRemarks() != "") {
-				pStmt.setString(13, "%" + param.getRemarks() + "%");
-			}
-			else {
-				pStmt.setString(13,"%");
 			}
 			// SQL文を実行し、結果表を取得する
 			ResultSet rs = pStmt.executeQuery();
@@ -124,20 +89,14 @@ public class QaDao {
 			// 結果表をコレクションにコピーする
 			while (rs.next()) {
 				Qa card = new Qa(
-				rs.getInt("NUMBER"),
-				rs.getString("NAME"),
-				rs.getString("NAMEFURIGANA"),
-				rs.getString("ENGLISHNAME"),
-				rs.getString("COMPANY"),
-				rs.getString("SECTION"),
-				rs.getString("POST"),
-				rs.getString("ZIPCODE"),
-				rs.getString("ADDRESS"),
-				rs.getString("MOBILENUMBER"),
-				rs.getString("TELEPHONENUMBER"),
-				rs.getString("FAX"),
-				rs.getString("MAILADDRESS"),
-				rs.getString("REMARKS")
+				rs.getInt("QUESTION_ID"),
+				rs.getString("DATE"),
+				rs.getString("ANSWERER"),
+				rs.getString("CATEGORY_ID"),
+				rs.getString("QUESTION"),
+				rs.getString("ANSWER"),
+				rs.getString("PAGEVIEW"),
+				rs.getString("REGISTANT")
 				);
 				cardList.add(card);
 			}
@@ -179,94 +138,54 @@ public class QaDao {
 			Class.forName("org.h2.Driver");
 
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/D-6/Doraemonno4jigenpoketto/database", "sa", "sa");
 
 			// SQL文を準備する
-			String sql = "insert into BC (NUMBER, NAME, NAMEFURIGANA, ENGLISHNAME, COMPANY, SECTION, POST, ZIPCODE, ADDRESS, MOBILENUMBER, TELEPHONENUMBER, FAX, MAILADDRESS, REMARKS)"
-					+ " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			String sql = "insert into QA (question_id, date, answerer, category_id, "
+					+ " question, answer, pageview, registant)"
+					+ " values (?, ?, ?, ?, ?, ?, 0, ?)";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
 
-			pStmt.setString(1, null);
-			if (card.getName() != null) {
-				pStmt.setString(2, card.getName());
+			pStmt.setInt(1, 0);
+			if (card.getDate() != null) {
+				pStmt.setDate(2, card.getDate());
 			}
 			else {
-				pStmt.setString(2, null);
+				pStmt.setDate(2, null);
 			}
-			if (card.getNamefurigana() != null) {
-				pStmt.setString(3, card.getNamefurigana());
+			if (card.getAnswerer() != null && card.getAnswerer() != "") {
+				pStmt.setString(3, card.getAnswerer());
 			}
 			else {
 				pStmt.setString(3, null);
 			}
-			if (card.getEnglishname() != null) {
-				pStmt.setString(4, card.getEnglishname());
+			if (card.getCategory_id() !=0) {
+				pStmt.setInt(4, card.getCategory_id());
 			}
 			else {
-				pStmt.setString(4, null);
+				pStmt.setInt(4, 0601001);
 			}
-			if (card.getCompany() != null) {
-				pStmt.setString(5, card.getCompany());
+			if (card.getQuestion() != null && card.getQuestion() != "") {
+				pStmt.setString(5, card.getQuestion());
 			}
 			else {
 				pStmt.setString(5, null);
 			}
-			if (card.getSection() != null) {
-				pStmt.setString(6, card.getSection());
+			if (card.getAnswer() != null && card.getAnswer() != "") {
+				pStmt.setString(6, card.getAnswer());
 			}
 			else {
 				pStmt.setString(6, null);
 			}
-			if (card.getPost() != null) {
-				pStmt.setString(7, card.getPost());
+			if (card.getRegistant() != null && card.getRegistant() != "") {
+				pStmt.setString(7, card.getRegistant());
 			}
 			else {
 				pStmt.setString(7, null);
 			}
-			if (card.getZipcode() != null) {
-				pStmt.setString(8, card.getZipcode());
-			}
-			else {
-				pStmt.setString(8, null);
-			}
-			if (card.getAddress() != null) {
-				pStmt.setString(9, card.getAddress());
-			}
-			else {
-				pStmt.setString(9, null);
-			}
-			if (card.getMobilenumber() != null) {
-				pStmt.setString(10,card.getMobilenumber());
-			}
-			else {
-				pStmt.setString(10, null);
-			}
-			if (card.getTelephonenumber() != null) {
-				pStmt.setString(11, card.getTelephonenumber());
-			}
-			else {
-				pStmt.setString(11, null);
-			}
-			if (card.getFax() != null) {
-				pStmt.setString(12, card.getFax());
-			}
-			else {
-				pStmt.setString(12, null);
-			}
-			if (card.getMailaddress() != null) {
-				pStmt.setString(13, card.getMailaddress());
-			}
-			else {
-				pStmt.setString(13, null);
-			}
-			if (card.getRemarks() != null) {
-				pStmt.setString(14, card.getRemarks());
-			}
-			else {
-				pStmt.setString(14, null);
-			}
+
 			// SQL文を実行する
 			if (pStmt.executeUpdate() == 1) {
 				result = true;
@@ -295,8 +214,7 @@ public class QaDao {
 	}
 
 
-
-	//初期の更新sql
+	//更新処理
 	// 引数cardで指定されたレコードを更新し、成功したらtrueを返す
 	public boolean update(Qa card) {
 		Connection conn = null;
@@ -307,92 +225,49 @@ public class QaDao {
 			Class.forName("org.h2.Driver");
 
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
-
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/D-6/Doraemonno4jigenpoketto/database", "sa", "sa");
 			// SQL文を準備する
-			String sql = "update BC set NAME=?, NAMEFURIGANA=?, ENGLISHNAME=?, COMPANY=?, SECTION=?, POST=?, ZIPCODE=?, ADDRESS=?, MOBILENUMBER=?, TELEPHONENUMBER=?, FAX=?, MAILADDRESS=?, REMARKS=? where NUMBER=?";
+			String sql = "update QA set DATE=?, ANSWERER=?, CATEGORY_ID=?, QUESTION=?, ANSWER=?, REGISTANT=? where QUESTION_ID=?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
-			if (card.getName() != null) {
-				pStmt.setString(1, card.getName());
+			if (card.getDate() != null) {
+				pStmt.setDate(1, card.getDate());
 			}
 			else {
-				pStmt.setString(1, null);
+				pStmt.setDate(1, null);
 			}
-			if (card.getNamefurigana() != null) {
-				pStmt.setString(2, card.getNamefurigana());
+			if (card.getAnswerer() != null) {
+				pStmt.setString(2, card.getAnswerer());
 			}
 			else {
 				pStmt.setString(2, null);
 			}
-			if (card.getEnglishname() != null) {
-				pStmt.setString(3, card.getEnglishname());
+			if (card.getCategory_id() != 0) {
+				pStmt.setInt(3, card.getCategory_id());
 			}
 			else {
-				pStmt.setString(3, null);
+				pStmt.setInt(3, 0601001);
 			}
-			if (card.getCompany() != null) {
-				pStmt.setString(4, card.getCompany());
+			if (card.getQuestion() != null) {
+				pStmt.setString(4, card.getQuestion());
 			}
 			else {
 				pStmt.setString(4, null);
 			}
-			if (card.getSection() != null) {
-				pStmt.setString(5, card.getSection());
+			if (card.getAnswer() != null) {
+				pStmt.setString(5, card.getAnswer());
 			}
 			else {
 				pStmt.setString(5, null);
 			}
-			if (card.getPost() != null) {
-				pStmt.setString(6, card.getPost());
+			if (card.getRegistant() != null) {
+				pStmt.setString(6, card.getRegistant());
 			}
 			else {
 				pStmt.setString(6, null);
 			}
-			if (card.getZipcode() != null) {
-				pStmt.setString(7, card.getZipcode());
-			}
-			else {
-				pStmt.setString(7, null);
-			}
-			if (card.getAddress() != null) {
-				pStmt.setString(8, card.getAddress());
-			}
-			else {
-				pStmt.setString(8, null);
-			}
-			if (card.getMobilenumber() != null) {
-				pStmt.setString(9, card.getMobilenumber());
-			}
-			else {
-				pStmt.setString(9, null);
-			}
-			if (card.getTelephonenumber() !=null) {
-				pStmt.setString(10, card.getTelephonenumber());
-			}
-			else {
-				pStmt.setString(10, null);
-			}
-			if (card.getFax() != null) {
-				pStmt.setString(11, card.getFax());
-			}
-			else {
-				pStmt.setString(11, null);
-			}
-			if (card.getMailaddress() != null) {
-				pStmt.setString(12, card.getMailaddress());
-			}
-			else {
-				pStmt.setString(12, null);
-			}
-			if (card.getRemarks() != null) {
-				pStmt.setString(13, card.getRemarks());
-			}
-			else {
-				pStmt.setString(13, null);
-			}
-			pStmt.setInt(14, card.getNumber());
+			pStmt.setInt(7, card.getQuestion_id());
 
 			// SQL文を実行する
 			if (pStmt.executeUpdate() == 1) {
@@ -420,8 +295,57 @@ public class QaDao {
 		// 結果を返す
 		return result;
 	}
+
+	//閲覧数を増やす
+	// 引数cardで指定されたレコードを更新し、成功したらtrueを返す
+	public boolean updatecount(Qa card) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/D-6/Doraemonno4jigenpoketto/database", "sa", "sa");
+			// SQL文を準備する
+			String sql = "update QA set PAGEVIEW=PAGEVIEW+1 where QUESTION_ID=?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+				pStmt.setInt(1, card.getQuestion_id());;
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
+		return result;
+	}
+
+
+
 	// 引数numberで指定されたレコードを削除し、成功したらtrueを返す
-		public boolean delete(int number) {
+		public boolean delete(int question_id) {
 			Connection conn = null;
 			boolean result = false;
 
@@ -430,14 +354,14 @@ public class QaDao {
 				Class.forName("org.h2.Driver");
 
 				// データベースに接続する
-				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/D-6/Doraemonno4jigenpoketto/database", "sa", "sa");
 
 				// SQL文を準備する
-				String sql = "delete from BC where NUMBER=?; ALTER TABLE BC drop column NUMBER; ALTER TABLE BC ADD NUMBER int primary key auto_increment first;";
+				String sql = "delete from QA where QUESTION_ID=?; ALTER TABLE QA drop column QUESTION_ID; ALTER TABLE QA ADD QUESTION_ID int primary key auto_increment first;";
 				PreparedStatement pStmt = conn.prepareStatement(sql);
 
 				// SQL文を完成させる
-				pStmt.setInt(1, number);
+				pStmt.setInt(1, question_id);
 
 				// SQL文を実行する
 				if (pStmt.executeUpdate() == 1) {
