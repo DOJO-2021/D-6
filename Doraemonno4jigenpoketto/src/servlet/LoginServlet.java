d4dd39da25f5574f7d80ccb4d88e682f466c3af1
向後海 <kougo@seplus2016.onmicrosoft.com>
2021-06-10 16:25:44 +0900

    servlet

--- Doraemonno4jigenpoketto/src/servlet/LoginServlet.java
+++ Doraemonno4jigenpoketto/src/servlet/LoginServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.UserDao;
+import model.User;
+//
 
 /**
  * Servlet implementation class LoginServlet
@@ -18,16 +24,40 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//ログインページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+				String id = request.getParameter("ID");
+				String pass = request.getParameter("PW");
 
+		//ログイン処理を行う
+		UserDAO iDao =new UserDAO() {
+		if (iDao.isLoginOK(id,pass)) { //ログイン成功
+			//セッションスコープにIDを格納する
+			HttpSession session = request.getSession();
+			session.setAttribute("id", new LoginUser(id));
+
+			// 編集サーブレットにリダイレクトする
+			response.sendRedirect("/Doraemonno4jigenpoketto/EditServlet");
+		}
+		else {// ログイン失敗
+			// リクエストスコープに、タイトル、メッセージ、戻り先を格納する
+			request.setAttribute("result",
+			new Result("ログイン失敗！", "IDまたはPWに間違いがあります。", "/Doraemonno4jigenpoketto/LoginServlet"));
+
+			// 結果ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+			dispatcher.forward(request, response);
+		}
+
+	}
+}
 }
